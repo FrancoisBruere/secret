e5d5d2f988005ffb7cdb2a34100e78cbdd7bfce3
FrancoisBruere <85226595+FrancoisBruere@users.noreply.github.com>
2021-07-01 09:43:07 +0200

    add bcrypt and salt to encryption

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
 const app = express();
-// const encrypt = require("mongoose-encryption");
-const md5 = require("md5");
-
+// const encrypt = require("mongoose-encryption"); changed for MD5
+// const md5 = require("md5"); changed for bcrypt
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -44,32 +45,41 @@ res.render("register");
 });
 
 app.post("/register", function(req, res){
-  const newUser = new User({
-    email: req.body.username,
-  //password: req.body.password
-  password: md5(req.body.password)
-  });
-  newUser.save(function(err){
-    if(err){
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash){
+    const newUser = new User({
+      email: req.body.username,
+    //password: req.body.password
+    //password: md5(req.body.password) changed to use bcrypt
+      password: hash
+    });
+    newUser.save(function(err){
+      if(err){
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
+  })
 });
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
+  //const password = md5(req.body.password); removed to use bcrypt
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
       console.log(err);
     }else{
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result){
+          if(result === true){
+              res.render("secrets");
+          }
+        });
+        //if(foundUser.password === password){
+          //res.render("secrets");
+        //}
       }
     }
   });
